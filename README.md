364f6abfbbe8766fa41d4f8c10e027c929c54636
Kovach Aleksey <kovach.aleksey19@yandex.ru>
2023-01-02 20:33:04 +1000

    release brain-gcd

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Cher-Cash/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/Cher-Cash/python-project-49/actions)
 <a href="https://codeclimate.com/github/Cher-Cash/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/7521bf7a17e92e14f0d6/maintainability" /></a>
-https://asciinema.org/a/549000
-https://asciinema.org/a/549001
+Example brain-even https://asciinema.org/a/549023
+Example brain-calc https://asciinema.org/a/549024
+Example brain-gcd https://asciinema.org/a/549046
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,39 @@
+#!usr/bin/env python3
+import prompt
+import random
+from ..cli import welcome_user
+from .brain_games import greet
+
+
+def gen():
+    number = random.randint(1, 20)
+    return number
+
+
+def calculation(first, second):
+    nod = 1
+    if first > second:
+        first, second = second, first
+    for i in range(1, first + 1):
+        if not first % i and not second % i:
+            nod = i
+    return nod
+
+
+def main():
+    greet()
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    for i in range(0,3):
+        f_number = gen()
+        s_number = gen()
+        print(f'Question: {f_number} {s_number}')
+        ans = prompt.string('Your answer: ')
+        check = calculation(f_number, s_number)
+        if check == int(ans):
+            print('Correct!')
+        else:
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{check}'")
+            print(f"Let's try again, {name}")
+            return 0
+    print(f'Congratulations, {name}')
